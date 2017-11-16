ca04cfae0f3f4687afb703bc1e927a3e72cb81ba
Hoplophile <piotr.ziebinski97@gmail.com>
2017-11-16 18:20:27 +0100

    Update

--- app/src/main/java/com/example/piotr/niewirtualnydziekanat/LoginActivity.java
+++ app/src/main/java/com/example/piotr/niewirtualnydziekanat/LoginActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.AsyncTask;
@@ -49,15 +48,15 @@ public class LoginActivity extends AppCompatActivity{
     private View mProgressView;
     private View mLoginFormView;
     private String albumNumber;
-    private String URL;
+    public String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button mEmailSignInButton = findViewById(R.id.sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
@@ -203,6 +202,7 @@ public class LoginActivity extends AppCompatActivity{
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 intent.putExtra("Album Number", albumNumber);
                 startActivity(intent);
+                mLoginFormView.setVisibility(View.GONE);
                 overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
             } else {
                 Toast.makeText(LoginActivity.this,
