358e34c02f3260c5e7c6c05313e0540d7cdc1fbc
Hoplophile <piotr.ziebinski97@gmail.com>
2018-02-26 18:29:54 +0100

    added navigation to map activity

--- app/src/main/java/com/example/piotr/niewirtualnydziekanat/MapActivity.java
+++ app/src/main/java/com/example/piotr/niewirtualnydziekanat/MapActivity.java
@@ -1,34 +1,43 @@
 package com.example.piotr.niewirtualnydziekanat;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.maps.android.ui.BubbleIconFactory;
+import com.google.maps.android.ui.IconGenerator;
 
 import java.util.ArrayList;
 
-/**
- * A styled map using JSON styles from a raw resource.
- */
-public class MapActivity extends AppCompatActivity
+public class MapActivity extends NavigationActivity
         implements OnMapReadyCallback {
 
+    private final Context context = MapActivity.this;
+    private final Activity activity = this;
     private static final String TAG = MapActivity.class.getSimpleName();
     private boolean mLocationPermissionGranted;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
@@ -39,14 +48,11 @@ public class MapActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_map);
 
-        // Get the SupportMapFragment and register for the callback
-        // when the map is ready for use.
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         /*
@@ -54,7 +60,18 @@ public class MapActivity extends AppCompatActivity
         mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         */
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
     }
 
     /**
@@ -65,7 +82,7 @@ public class MapActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {                                                   //TODO: add buildings IDs
         
         try {
-            // Customise the styling of the base map using a JSON object defined
+            // Customise the styling of the base map using a JSON object defined                    //TODO: button changing map type
             // in a raw resource file.
             boolean success = googleMap.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
@@ -77,13 +94,18 @@ public class MapActivity extends AppCompatActivity
         } catch (Resources.NotFoundException e) {
             Log.e(TAG, "Can't find style. Error: ", e);
         }
-        // Position the map's camera near Sydney, Australia.
+        // Position the map's camera on AGH
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(50.06666, 19.914048)));
         getDeviceLocation();
         updateLocationUI();
         map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-        map.addMarker(new MarkerOptions().position(new LatLng(50.064546, 19.923313)).title("A0"));
+        IconGenerator iconGenerator = new IconGenerator(this);
+        MarkerOptions markerOptions = new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon("A-0")))
+                .position(new LatLng(50.064546, 19.923313));
+        map.addMarker(markerOptions);
     }
 
     private void getLocationPermission() {
